27dc002ac751425de5b4b38b41ada57a50133ac4
Owen Elliott <owen.bruce.elliott@gmail.com>
2022-07-11 10:07:29 -0400

    few more times

--- thing.sql
+++ thing.sql
@@ -7,5 +7,5 @@ CREATE TABLE FLUIDS
 NAME VARCHAR(25));
 INSERT INTO FLUIDS (NAME) VALUES ('Dihydrogen Oxide'), ('Helium'), ('Hydrogen'), ('Oxygen'), ('Tritium'), ('Xenon');
 DROP TABLE thing;
-CREATE TABLE thing (MemberID INT PRIMARY KEY, Credits INT DEFAULT 0, Items VARCHAR(25)[] DEFAULT ARRAY[]::VARCHAR(25)[], Schematics integer[] DEFAULT ARRAY[]::integer[]);
-INSERT INTO thing (MemberID) VALUES (6)
+CREATE TABLE "345" (MemberID INT PRIMARY KEY, Credits INT DEFAULT 0, Items VARCHAR(25)[] DEFAULT ARRAY[]::VARCHAR(25)[], Schematics integer[] DEFAULT ARRAY[]::integer[]);
+INSERT INTO "345" (MemberID) VALUES (6)
