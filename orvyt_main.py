715628fea8adf2d5b5700d4b578974366adf6a84
Owen Elliott <owen.bruce.elliott@gmail.com>
2022-07-12 11:11:35 -0400

    v1 test with database

--- orvyt_main.py
+++ orvyt_main.py
@@ -34,6 +34,9 @@ GM={}
 
 @client.event
 async def on_ready():
+    for guild in client.guilds:
+        for role in guild.roles:
+            if role.name=='Game Master': GM[guild.id]=role.id
     print('Orvyt_Online!')
 
 @client.event
@@ -62,30 +65,21 @@ async def help(ctx, display:discord.Option(bool,"display command result to other
     longmsg+='masterlist\*: the GM can add or remove possible items\n view: see player\'s inventories and schematics!\n credit\*\*: exhange credits!\n \*: GM only'
     await ctx.respond(longmsg, ephemeral=not display)
 
-@client.slash_command()
-async def commmitdb(ctx, description="DO NOT TOUCH UNLESS YOU ARE OWEN"):
+@client.slash_command(description="DO NOT TOUCH UNLESS YOU ARE OWEN")
+async def commmitdb(ctx):
     cursor=conn.cursor()
     for guild in client.guilds:
         query=sql.SQL('CREATE TABLE {guildID} (MemberID BIGINT PRIMARY KEY, Credits INT DEFAULT 0, Items VARCHAR(25)[] DEFAULT ARRAY[]::VARCHAR(25)[], Schematics integer[] DEFAULT ARRAY[]::integer[])').format(guildID=sql.Identifier(str(guild.id)))
         cursor.execute(query)
-        for role in guild.roles:
-            if role.name=='Game Master': GM[guild.id]=role.id
         for member in guild.members:
             query=sql.SQL('INSERT INTO {guildID} (MemberID) VALUES (%s)').format(guildID=sql.Identifier(str(guild.id)))
             cursor.execute(query,(member.id,))
+    await ctx.respond("Database has been commited, or duplicated if you're stupid")
     conn.commit()
 
 @client.slash_command(description="responds with a random number between 1 and 10")
 async def dten(ctx, display:discord.Option(bool,"display command result to others")=True):
     await ctx.respond(str(random.randint(1,10)), ephemeral=not display)
-    
-@client.slash_command(description='logs all guild information (GM only)')
-async def logall(ctx, display:discord.Option(bool,"display command result to others")=True):
-    if ctx.interaction.user.get_role(PLAYERS[ctx.interaction.guild.id]['GM'] !=None) or ctx.interaction.user.id==693848229896388669:
-        embed=discord.Embed.from_dict(PLAYERS[ctx.interaction.guild.id])
-        await ctx.respond(embed=embed, ephemeral=not display)
-    else:
-        await ctx.respond('classified', ephemperal=True)
 
 @client.slash_command(description='gives item to player\'s inventory')
 async def give(ctx, user:discord.Option(discord.Member, "who to give to."), category:discord.Option(str,choices=['Metal(M)','Fluid(F)','Irradiated(R)','Component(C)','Item(I)', 'Weapon(W)', 'Schematic(S)']), number:discord.Option(int, "what serial number of item", min_value=1)):
@@ -201,7 +195,7 @@ viewCmnds=client.create_group('view')
 @viewCmnds.command(description="view player's items")
 async def items(ctx, user:discord.Option(discord.Member, "whose items"), display:discord.Option(bool,"display command result to others")=True):
     cursor=conn.cursor()
-    query=sql.SQL('SELECT Items FROM {guildID} where MemberID=%s').format(guildID=sql.Identifier(str(user.guild.id)))
+    query=sql.SQL('SELECT Items FROM {} where MemberID=%s').format(sql.Identifier(str(user.guild.id)))
     cursor.execute(query,(user.id,))
     inventory=cursor.fetchone()[0]
     if len(inventory):
@@ -212,7 +206,7 @@ async def items(ctx, user:discord.Option(discord.Member, "whose items"), display
 @viewCmnds.command(description="view player's schematics")
 async def schematics(ctx, user:discord.Option(discord.Member, "whose schemaitcs"), display:discord.Option(bool,"display command result to others")=True):
         cursor=conn.cursor()
-        query=sql.SQL('SELECT Schematics FROM {guildID} where MemberID=%s').format(guildID=sql.Identifier(str(user.guild.id)))
+        query=sql.SQL('SELECT Schematics FROM {} where MemberID=%s').format(sql.Identifier(str(user.guild.id)))
         cursor.execute(query,(user.id,))
         inventory=cursor.fetchone()[0]
         if len(inventory)>0:
@@ -224,23 +218,35 @@ credit=client.create_group('credit')
 
 @credit.command(description="give money to player")
 async def give(ctx, user:discord.Option(discord.Member, "whom to give money to."), wealth:discord.Option(int,"how much money to give", min_value=0)):
-    PLAYERS[user.guild.id][user.id]['Credits']+=wealth
-    if ctx.interaction.user.get_role(PLAYERS[ctx.interaction.guild.id]['GM'])!= None:
-        await ctx.respond(f'{user.name} now has {PLAYERS[user.guild.id][user.id]["Credits"]} credits')
+    cursor=conn.cursor()
+    query=sql.SQL('UPDATE {} SET Credits=Credits+%s WHERE MemberID=%s').format(sql.Identifier(str(user.guild.id)))
+    cursor.execute(query,(wealth,user.id))
+    if ctx.interaction.user.get_role(GM[ctx.interaction.guild.id])!= None:
+        conn.commit()
+        await ctx.respond(f'{user.name} was given {wealth} credits')
     else:
-        PLAYERS[ctx.interaction.guild.id][ctx.interaction.user.id]['Credits']-=wealth
+        query=sql.SQL('UPDATE {} SET Credits=Credits+%s WHERE MemberID=%s').format(sql.Identifier(str(ctx.interaction.guild.id)))
+        cursor.execute(query,(wealth,ctx.interaction.user.id))
+        conn.commit()
         await ctx.respond(f'{ctx.interaction.user.name} gave {user.name} {wealth} credits')
 
 @credit.command(description="take player's money (GM only)")
 async def remove(ctx, user:discord.Option(discord.Member, 'who\'s money to take'), wealth:discord.Option(int,"how much money to give", min_value=0)):
-    if ctx.interaction.user.get_role(PLAYERS[ctx.interaction.guild.id]['GM'])!= None:
-        PLAYERS[user.guild.id][user.id]['Credits']-=wealth
-        await ctx.respond(f'{user.name} now has {PLAYERS[user.guild.id][user.id]["Credits"]} credits')
+    if ctx.interaction.user.get_role(GM[ctx.interaction.guild.id])!= None:
+        cursor=conn.cursor()
+        query=sql.SQL('UPDATE {} SET Credits=Credits+%s WHERE MemberID=%s').format(sql.Identifier(str(user.guild.id)))
+        cursor.execute(query,(wealth,user.id))
+        conn.commit()
+        await ctx.respond(f'{user.name} lost {wealth} credits')
     else:
         await ctx.respond('You do not have permission to rob people.', ephemeral=True)
 
 @credit.command(description="view player's money")
 async def view(ctx, user:discord.Option(discord.Member, 'who\'s money to view'), displayed:discord.Option(bool,"display command result to others")=True):
-    await ctx.respond(f'{user.name} has {PLAYERS[user.guild.id][user.id]["Credits"]} Credits', ephemeral= not displayed)
+    cursor=conn.cursor()
+    query=sql.SQL('SELECT Credits FROM {} WHERE MemberID=%s').format(sql.Identifier(str(user.guild.id)))
+    cursor.execute(query, (user.id,))
+    creditNum=cursor.fetchone()[0]
+    await ctx.respond(f'{user.name} has {creditNum} Credits', ephemeral= not displayed)
 
 client.run(os.environ['ORVYT_TOKEN'])
